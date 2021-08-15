d5463f969615e35fb8c064e8430683581b6eb7ff
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2021-08-15 15:15:15 +0800

    v0.7

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	app := &cli.App{
 		Name:    "CDNDrive-go",
 		Usage:   "Make Picbeds Great Cloud Storages!",
-		Version: "v0.6",
+		Version: "v0.7",
 		Authors: []*cli.Author{
 			&cli.Author{
 				Name: "猫村あおい",
